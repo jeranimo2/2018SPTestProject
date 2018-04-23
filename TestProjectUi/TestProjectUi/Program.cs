282daedc5d583401c667cc519ffdf340db8a8c49
prorammer <dpina21@actx.edu>
2018-04-23 17:00:10 -0500

    begining of code

---
+++ TestProjectUi/TestProjectUi/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProjectUi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // the following is to show  hello to the end user
+            Console.WriteLine("Hello world!!!");
+        }
+    }
+}
